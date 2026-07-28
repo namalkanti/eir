2684b3539ccedf8f2a16728837dfb8657882b35c
Niraj Amalkanti <namalkanti@gmail.com>
2026-07-27 23:28:17 -0400

    Commiting initial flake and configuration

---
+++ configuration.nix
@@ -0,0 +1,5 @@
+{ pkgs, ... }:
+
+{
+  networking.hostName = "eir-recovery";
+}
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Eir - Custom NixOS Recovery USB";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.recovery = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+        ./configuration.nix
+      ];
+    };
+  };
+}
