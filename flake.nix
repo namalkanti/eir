8a8323e675c839763facbf289bd63516f9acb33a
Niraj Amalkanti <namalkanti@gmail.com>
2026-07-28 22:27:25 -0400

    Updating flake

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
   };
 
   outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = self.nixosConfigurations.recovery.config.system.build.isoImage;
+
     nixosConfigurations.recovery = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
